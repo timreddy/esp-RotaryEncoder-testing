b1655bd0ce873f00ac0266aea7c6ba9d7c7ba35a
Tim Reddy <treddy@gmail.com>
2024-10-07 16:52:42 -0400

    Setting up initial files

---
+++ main/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+#include "RotaryEncoder.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+extern "C" void app_main() {
+  ;
+}

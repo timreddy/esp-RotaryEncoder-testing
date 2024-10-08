3c93dc40017ab8088fe104bfb9a4cb92b50a2b38
Tim Reddy <treddy@gmail.com>
2024-10-07 22:41:06 -0400

    Rewrote test code to simply create an encoder and watch the values over time

--- main/main.cpp
+++ main/main.cpp
@@ -12,6 +12,13 @@
 #define ROTARY_PIN1 GPIO_NUM_26
 #define ROTARY_PIN2 GPIO_NUM_25
 
+#define TAG "main"
+
 extern "C" void app_main() {
-  RotaryEncoder speedDial(ROTARY_PIN1, ROTARY_PIN2);
+  RotaryEncoder dial(ROTARY_PIN1, ROTARY_PIN2, -250, 250);
+
+  while(1) {
+    ESP_LOGI(TAG, "Count: %d", dial.getCount());
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
 }
